5ef0dddf1b7dcd0388dad15aab6a285f9b597720
t-mak <makowski.t91@gmail.com>
2022-12-28 03:41:54 +0900

    Add webscrapping module

--- coding.py
+++ coding.py
@@ -283,3 +283,74 @@ print(html[start_index:end_index])
 # of each pattern should be a “<” (the start of an HTML tag) or a newline character, and you should remove any extra spaces or newline
 # characters from the resulting text using the string .strip() method.
 
+
+
+
+
+###Web scrapping using Beautiful Soup library
+
+# from bs4 import BeautifulSoup
+# from urllib.request import urlopen
+# url = "http://olympus.realpython.org/profiles/dionysus"
+
+# page = urlopen(url)
+# html = page.read().decode("utf-8")
+# soup = BeautifulSoup(html, "html.parser")
+
+# print(soup)
+
+# print(soup.get_text())
+
+# soup.find_all("img")
+# soup.find_all("img", src="/static/dionysus.jpg")
+
+###Web scrapping using Mechanical Soup library
+
+# import mechanicalsoup
+# browser = mechanicalsoup.Browser()
+# url = "http://olympus.realpython.org/login"
+# page = browser.get(url)
+
+# print(page.soup)
+
+# import mechanicalsoup
+
+# browser = mechanicalsoup.Browser()
+# url = "http://olympus.realpython.org/login"
+# login_page = browser.get(url)
+# login_html = login_page.soup
+
+# form = login_html.select("form")[0]
+# form.select("input")[0]["value"] = "zeus"
+# form.select("input")[1]["value"] = "ThunderDude"
+
+# profiles_page = browser.submit(form, login_page.url)
+
+# print(profiles_page.url)
+
+# links = profiles_page.soup.select("a")
+
+# base_url = "http://olympus.realpython.org"
+
+# for link in links:
+#     address = base_url + link["href"]
+#     text = link.text
+#     print(f"{text}: {address}")
+
+
+import mechanicalsoup
+import time
+
+browser = mechanicalsoup.Browser()
+
+
+for i in range(4):
+    page = browser.get("http://olympus.realpython.org/dice")
+    tag = page.soup.select("#result")[0]
+    result = tag.text
+    print(f"The result of your dice roll is: {result}")
+    time.sleep(30)
+
+if i < 3:
+    time.sleep(30)
+
